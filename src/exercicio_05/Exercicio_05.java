8af041ad1137a6c3e441c44c945e52e0b08f4f0a
LorenzoBruscato <lorenzobruscato6@gmail.com>
2025-05-28 20:55:42 -0300

    Exercício_05

---
+++ src/exercicio_05/Exercicio_05.java
@@ -0,0 +1,16 @@
+package exercicio_05;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Lorenzo
+ */
+public class Exercicio_05 {
+
+    public static void main(String[] args) {
+        int numero;
+        numero = 10;
+        JOptionPane.showMessageDialog(null, numero);
+    }
+}
